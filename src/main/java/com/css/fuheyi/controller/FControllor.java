08ad61c7566213171f3cf2ef4c88ac58a9bfd42d
fuhy <fhy517714@163.com>
2019-01-08 13:22:33 +0800

    测试一下修改

--- src/main/java/com/css/fuheyi/controller/FControllor.java
+++ src/main/java/com/css/fuheyi/controller/FControllor.java
@@ -19,7 +19,7 @@ import java.util.Set;
  * Created by fhy51 on 2018/8/10.e
  */
 @RestController
-@RequestMapping("/hiii")
+@RequestMapping("/hiiii")
 public class FControllor {
     @Autowired
     private ObjectMapper mapper;
